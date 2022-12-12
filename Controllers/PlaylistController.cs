f541be5530e9fb9fa282155ce3761919c934cd39
Amir Nilsson <amir.nilsson@king.com>
2022-12-12 17:35:01 +0100

    CRUD operations working in swagger

---
+++ Controllers/PlaylistController.cs
@@ -0,0 +1,42 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Grupp4.Services;
+using Grupp4.Models;
+
+namespace Grupp4.Controllers; 
+
+[Controller]
+[Route("api/[controller]")]
+public class PlaylistController: Controller {
+
+    private readonly MongoDbService _mongoDbService;
+
+    public PlaylistController(MongoDbService mongoDbService) {
+        _mongoDbService = mongoDbService;
+    }
+
+    [HttpGet]
+    public async Task<List<Playlist>> Get() {
+        return await _mongoDbService.GetAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] Playlist playlist) {
+        await _mongoDbService.CreateAsync(playlist);
+        return CreatedAtAction(nameof(Get), new { id = playlist.Id }, playlist);
+    }
+
+    [HttpPut("{id}")]
+
+    public async Task<ActionResult> AddToPlaylist(string id, [FromBody] string movieIds) {
+        await _mongoDbService.AddToPlaylistAsync(id, movieIds);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(string id) {
+        await _mongoDbService.DeleteAsync(id);
+        return NoContent();
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
+using Grupp4.Models;
+using Grupp4.Services;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDB"));
+
+builder.Services.AddSingleton<MongoDbService>();
+
+
+
+
 // Add services to the container.
+/*
+
+// add mongoclient
+MongoClient client_Amir = new MongoClient("mongodb+srv://Amir:wjbn7WrAW3m5RrpU@amircluster.bux2ihe.mongodb.net/?retryWrites=true&w=majority");
+
+List<string> databases = client_Amir.ListDatabaseNames().ToList();
+*/
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
